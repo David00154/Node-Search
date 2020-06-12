ce4dda0ed63144b950c57d14f5825f8fa0271ba3
David00154 <56704811+David00154@users.noreply.github.com>
2020-06-11 20:52:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -20,9 +20,10 @@ app.post('/', (req, res) => {
   	if (err) throw err;
   })
 });
-
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+const port = process.env.PORT || 5000
+app.listen(port, (err) => {
+if (err) throw err;
+  console.log(port);
+})
 
 
