31e72ad4452a210ce5130aeafd66496d6826c25f
David00154 <56704811+David00154@users.noreply.github.com>
2020-06-11 19:59:21 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post('/', (req, res) => {
+  const text = req.body.text;
+  fetch(`http://api.serpstack.com/search?access_key=b77e3733c11b0ad617198a9e71c38a22&query=${text}`)
+  .then(response => response.json())
+  .then(dataFetched => {
+  	res.json(dataFetched);
+  })
+  .catch(err => {
+  	if (err) throw err;
+  })
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+
